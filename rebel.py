dc236d2bd71b8c43c1aac52993fda033057f93ea
emalvaradog <emanuel.alvarado.g@gmail.com>
2024-06-13 00:38:08 -0600

    fix

--- rebel.py
+++ rebel.py
@@ -3,6 +3,7 @@ import os
 import time
 import json
 import streamlit as st
+from setup import SingletonAssistant
 
 # FUNCTIONS
 def get_projects(prompt):
@@ -33,121 +34,29 @@ def append_message(role, content):
   })
 
 
-# SETUP
-
-
-company = "De Acero"
-
-api_key = st.secrets.api_key
-
-# print("api_key: ", api_key)
-
-
-# ---------------------------------------------
-
-## IMPORTANT: KEEP IT AS A CONVERSTATION, NOT AS A BULK DATA LOAD.
-## IMPORTANT: KEEP IT AS A CONVERSTATION, NOT AS A BULK DATA LOAD.
-## IMPORTANT: KEEP IT AS A CONVERSTATION, NOT AS A BULK DATA LOAD.
-
-## change initial instructions to keep it as a conversation
-## change initial instructions to keep it as a conversation
-## change initial instructions to keep it as a conversation
-
-# ---------------------------------------------
-
-client = openai.Client(api_key=api_key)
-
-assistant = client.beta.assistants.create(
-  name="Rebel",
-  instructions=f"Eres un Traductor de Datos empresarial para {company}. Tu rol es ayudar a los usuarios de negocios a validar sus ideas de proyectos dentro de su compañía mediante UN DIALOGO INTERACTIVO FLUIDO para identificar proyectos existentes y asistir en la creación de nuevos proyectos si es necesario. Al interactuar con un usuario DEBES: Iniciar un diálogo haciendo preguntas clarificadoras para comprender las necesidades y objetivos del usuario de forma progresiva. NUNCA SOLICITANDO TODO DE PRIMERA MANO. Dialogar sobre los proyectos relevantes ya existentes, si los hay, e indagar si el usuario necesita más asistencia o detalles adicionales. Si no se identifican proyectos adecuados o el usuario desea iniciar un nuevo proyecto, conversar para recabar de manera PROGRESIVA Y AMIGABLE la información necesaria: nombre del proyecto, descripción, área de impacto y descripción del problema. En cada paso, debes ser extremadamente útil y proactivo, guiando al usuario a través del proceso con un enfoque conversacional claro y directo.",
-  model="gpt-4",
-  temperature=0.6,
-  tools=[
-    {
-      "type": "function",
-      "function": {
-        "name": "get_projects",
-        "description": "Get projects related to the user's query",
-        "parameters": {
-          "type": "object",
-          "properties":{
-            "prompt": {
-              "type": "string",
-              "description": "User input for project search"
-            }
-          },
-          "required": ["prompt"]
-        }
-      }
-    },
-    {
-      "type": "function",
-      "function": {
-        "name": "create_project",
-        "description": "Create a new project with the provided information",
-        "parameters": {
-          "type": "object",
-          "properties":{
-            "name": {
-              "type": "string",
-              "description": "Project name"
-            },
-            "description": {
-              "type": "string",
-              "description": "Project description"
-            },
-            "impact": {
-              "type": "string",
-              "description": "Project impact. Values: savings (significa que el proyecto ahorra dinero), income (significa que el proyecto aumenta ingreso), other (significa que el proyecto tiene otro tipo de impacto)"
-            },
-            "problem": {
-              "type": "object",
-              "properties": {
-                "what": {
-                  "type": "string",
-                  "description": "What is the problem?"
-                },
-                "why": {
-                  "type": "string",
-                  "description": "Why is it a problem?"
-                },
-                "fiveWhys": {
-                  "type": "array",
-                  "items": {
-                    "type": "string"
-                  },
-                  "description": "Five whys analysis"
-                }
-              }
-            }
-          },
-          "required": ["prompt"]
-        },
-      }
-    }
-  ]
-)
-
 messages = []
-
-thread = client.beta.threads.create()
-
-st.title(f"Rebel - Your Company Data Translator")
 
 if "messages" not in st.session_state:
   st.session_state.messages = messages
 
+# SETUP
+api_key = st.secrets.api_key
 
-for message in st.session_state.messages:
+client, assistant, thread = SingletonAssistant.get_instance(api_key=api_key)
+
+st.title(f"Rebel - Your Company Data Translator")
+
+for message  in st.session_state.messages:
   if message["role"] == "user":
     with st.chat_message("user"):
       st.write(message["content"])
+      
   elif message["role"] == "assistant":
     with st.chat_message("assistant"):
-      st.write(message['content'])
-
+      st.write(message["content"])
 
 user_input = st.chat_input(placeholder="Consulta a tu Data Translator")
+
 
 if user_input:
 
@@ -170,6 +79,7 @@ if user_input:
   if run.status == "completed":
     messages = client.beta.threads.messages.list(thread_id=thread.id)
     data = messages.data
+
     append_message("assistant", data[0].content[0].text.value)
 
     with st.chat_message("assistant"):
@@ -219,8 +129,10 @@ if user_input:
     if run.status == "completed": 
       messages = client.beta.threads.messages.list(thread_id=thread.id)
       data = messages.data
-      # print(data[0].content[0].text.value)
+
       append_message("assistant", data[0].content[0].text.value)
+
+      # print(data[0].content[0].text.value)
 
       with st.chat_message("assistant"):
         st.write(data[0].content[0].text.value)
